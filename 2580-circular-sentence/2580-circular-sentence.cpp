4436922c07a4963d80a513765bb0b7ce6c61794d
Aryan Singh <149005346+Aryan31457@users.noreply.github.com>
2024-11-02 15:37:28 +0530

    Time: 0 ms (100%), Space: 8.2 MB (83.91%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.cpp
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -5,16 +5,9 @@ public:
 
         if(sentence[0]!=sentence[n-1]){return false;}
         for(int i=0;i<n;i++){
-
-            char temp=sentence[i];
-            while(i<n&&sentence[i]!=' '){
-                temp=sentence[i];
-                i++;
+            if(sentence[i]==' '){
+                if(sentence[i-1]!=sentence[i+1]){return false;} 
             }
-            if(i+1<n){
-                if(temp!=sentence[i+1]){return false;} 
-            }
-
         }
         return true;
     }
