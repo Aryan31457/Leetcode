a5e621f644263a0aea6c339cb638c963b5021619
Aryan Singh <149005346+Aryan31457@users.noreply.github.com>
2024-12-19 21:53:38 +0530

    Time: 21 ms (32.04%), Space: 40.6 MB (7.99%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,47 +1,18 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-      int i=0,j=0;
-      int n=nums.size();
-      int sum=0;
-      int ans=0;
-       if (goal == 0) {
-            int zeroCount = 0;
-            for (int num : nums) {
-                if (num == 0) {
-                    zeroCount++;
-                } else {
-                    ans += zeroCount * (zeroCount + 1) / 2;
-                    zeroCount = 0;
-                }
-            }
-            ans += zeroCount * (zeroCount + 1) / 2;
+        unordered_map<int,int>mappi;
+        int ans=0;
+        int sum=0;
+        int n=nums.size();
+        mappi[0]=1;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+           
+            ans+=mappi[sum-goal];
+            
+            mappi[sum]++;
         }
-        else{
-      while(i<n){
-       
-        sum+=nums[i];
-        if(sum==goal){
-            int cnt=1;
-            int temp=i+1;
-            while(temp<n && nums[temp]==0){
-                temp++;
-                cnt++;
-            }
-        
-        int chk=0;
-        while(j<=i&&sum==goal){
-            sum-=nums[j];
-            j++;
-            chk++;
-        }
-
-        ans+=chk*cnt;
-        }
-        i++;
-      }  
-        }
-     
-      return ans;
-    }
+        return ans;
+     }
 };
