42711176f37a53822975dee658ddb32cdc9747d8
Aryan Singh <149005346+Aryan31457@users.noreply.github.com>
2024-12-20 12:36:09 +0530

    Time: 3 ms (74.44%), Space: 10.9 MB (21.88%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int i = 0, j = 0;
-        int n = s.size();
-        int ans = 0;
-        int maxi = 0;
-        unordered_map<char, int> mappi;
-
-        while (i < n) {
+        int i=0,j=0;
+        int n=s.size();
+        int ans=0;
+        int maxi=0;
+        unordered_map<char,int>mappi;
+        while(i<n){
             mappi[s[i]]++;
-            maxi = max(maxi, mappi[s[i]]);
-            
-            while (i - j + 1 - maxi > k) {
+            maxi=max(maxi,mappi[s[i]]);
+            int diff=i-j+1 - maxi;
+            while(i-j+1 - maxi >k){
                 mappi[s[j]]--;
                 j++;
-                maxi = 0;
-                for (auto it : mappi) {
-                    maxi = max(maxi, it.second);
+                maxi=0;
+                for(auto i:mappi){
+                    maxi=max(maxi,i.second);
                 }
             }
-
-            ans = max(ans, i - j + 1);
+            ans=max(ans,i-j+1);
             i++;
         }
-
         return ans;
     }
 };
