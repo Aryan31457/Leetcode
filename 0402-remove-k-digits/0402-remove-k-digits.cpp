7738e9006c52ad6cc40e446ba744f65f223080e1
Aryan Singh <149005346+Aryan31457@users.noreply.github.com>
2024-12-21 10:45:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,31 +1,41 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        int n = num.length();
-        stack<char> st;
-        for(int i = 0; i < n; i++){
-            int ch = num[i] - '0';
-            while(k > 0 && !st.empty() && (st.top() - '0') > ch){
-                k--;
-                st.pop();
-            }
+       string ans;
+       stack<int>st;
+       int n=num.size(); 
+       for(int i=0;i<n;i++){
 
-            st.push(num[i]);
+        while(!st.empty() && st.top()>(num[i]-'0') && k>0){
+            st.pop();
+            k--;
         }
-        while(k--) st.pop();
+        st.push(num[i]-'0');
+       }
 
-        string ans = "";
-        while(!st.empty()){
-             ans += st.top();
-             st.pop();
-        }
-        reverse(ans.begin(), ans.end());
-        int i = 0;
-        for(; i < ans.length(); i++){
-            if(ans[i] != '0') {break;}
-        }
+       while(k>0 && !st.empty()){
+        st.pop();
+        k--;
+       }
 
-        if(ans.size()-i == 0){ return "0";}
-         return ans.substr(i);
+       while(!st.empty()){
+        int topi=st.top();
+        ans=to_string(topi)+ans;
+        st.pop();
+       }
+        int i=0;
+       while(i<n && ans[i]=='0'){
+        i++;
+       }
+       string result;
+       n=ans.size();
+       while(i<n){
+        result+=ans[i];
+        i++;
+       }
+       if(result.size()==0){
+        result+='0';
+       }
+       return result; 
     }
 };
