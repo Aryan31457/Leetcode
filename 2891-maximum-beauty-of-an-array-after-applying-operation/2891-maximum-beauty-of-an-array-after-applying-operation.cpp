feb795c5ecf2342a2749fa9bc689488634dc54ed
Aryan Singh <149005346+Aryan31457@users.noreply.github.com>
2024-12-23 11:20:35 +0530

    Time: 11 ms (85.69%), Space: 111.6 MB (25.21%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
         int ans=0;
-        int i=0,j=0;
+        int maxi=0;
         int n=nums.size();
-        while(i<n){
-            while(j<i && nums[i]-nums[j]>2*k){
-                j++;
+    
+        for(int i=0;i<n;i++){
+            maxi=max(maxi,nums[i]);
+        }
+
+        vector<int>mappi(maxi+1,0);
+        for(int i=0;i<n;i++){
+            int start=nums[i];
+            mappi[(max(0,start-k))]++;
+
+            if(start+k+1<=maxi){
+                mappi[start+k+1]--;
             }
-            ans=max(i-j+1,ans);
-            i++;
+        }
+        int sum=0;
+        for(int i=0;i<maxi+1;i++){
+            sum+=mappi[i];
+            ans=max(ans,sum);
+            
         }
         return ans;
     }
