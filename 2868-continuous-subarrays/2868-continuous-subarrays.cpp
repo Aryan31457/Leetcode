46a1d977f16f3fd841e1fb179e5d2e18f83024fd
Aryan Singh <149005346+Aryan31457@users.noreply.github.com>
2024-12-23 11:55:02 +0530

    Time: 226 ms (19.69%), Space: 156 MB (7.32%) - LeetHub

--- 2868-continuous-subarrays/2868-continuous-subarrays.cpp
+++ 2868-continuous-subarrays/2868-continuous-subarrays.cpp
@@ -1,45 +1,16 @@
 class Solution {
 public:
     long long continuousSubarrays(vector<int>& nums) {
-        unordered_map<int, int> mappi;
+        multiset<int> window;
         long long ans = 0;
         int n = nums.size();
         int i = 0, j = 0;
-        int maxi = 0, mini = INT_MAX;
 
         while (i < n) {
-            mappi[nums[i]]++;
-            maxi = max(maxi, nums[i]);
-            mini = min(mini, nums[i]);
+            window.insert(nums[i]);
 
-            while (maxi - mini > 2) {
-                if (nums[j] == maxi) {
-                    int temp = INT_MIN;
-                    mappi[nums[j]]--;
-                    if (mappi[nums[j]] == 0)
-                        mappi.erase(nums[j]);
-
-                    for (const auto& [key, count] : mappi) {
-                        if (count > 0)
-                            temp = max(temp, key);
-                    }
-                    maxi = temp;
-                } else if (nums[j] == mini) {
-                    int temp = INT_MAX;
-                    mappi[nums[j]]--;
-                    if (mappi[nums[j]] == 0)
-                        mappi.erase(nums[j]);
-
-                    for (const auto& [key, count] : mappi) {
-                        if (count > 0)
-                            temp = min(temp, key);
-                    }
-                    mini = temp;
-                } else {
-                    mappi[nums[j]]--;
-                    if (mappi[nums[j]] == 0)
-                        mappi.erase(nums[j]);
-                }
+            while (*window.rbegin() - *window.begin() > 2) {
+                window.erase(window.find(nums[j]));
                 j++;
             }
 
