8c8498cf93b728fa6cdce2fa83575147f1d1761f
Aryan Singh <149005346+Aryan31457@users.noreply.github.com>
2024-12-26 10:43:49 +0530

    Time: 0 ms (100%), Space: 21 MB (44.31%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int i=1;
+        int n=ratings.size();
+        int ans=n;
+        while(i<n){
+            int cnt=0;
+
+            while(i<n && ratings[i-1]==ratings[i]){
+                i++;
+            }
+
+            while(i<n && ratings[i-1]<ratings[i]){
+                cnt++;
+                ans+=cnt;
+                i++;
+            }
+            int cnt1=0;
+            while(i<n && ratings[i-1]>ratings[i]){
+                cnt1++;
+                ans+=cnt1;
+                i++;
+            }
+            ans-=min(cnt,cnt1);
+        }
+        return ans;
+    }
+};
