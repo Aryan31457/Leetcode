5bca3106a7b0a15c60e20e64b71574dc6eb76969
Aryan Singh <149005346+Aryan31457@users.noreply.github.com>
2024-12-30 18:55:00 +0530

    Time: 85 ms (51.48%), Space: 120.2 MB (29.54%) - LeetHub

---
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void bfs( queue<pair<int,int>>&q,vector<vector<int>>& adj,vector<vector<int>>&visited){
+        int m = adj.size(), n = adj[0].size();
+        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int cnt=1;
+        while(!q.empty()){
+            int x=q.size();
+            for(int i=0;i<x;i++){
+                auto [row, col] = q.front();
+                q.pop();
+                for (auto [dx, dy] : directions) {
+                    int newRow = row + dx;
+                    int newCol = col + dy;
+
+if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && visited[newRow][newCol]==0 ) {
+                        adj[newRow][newCol] = cnt;
+                        visited[newRow][newCol]=1;
+                        q.push({newRow, newCol});
+                    }
+                }
+            }
+           cnt++;
+        }
+    }
+    vector<vector<int>> highestPeak(vector<vector<int>>& adj) {
+        int m = adj.size(), n = adj[0].size();
+        vector<vector<int>>result(m,vector<int>(n,0));
+         queue<pair<int,int>>q;
+        vector<vector<int>>visited(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                result[i][j]=adj[i][j];
+                if(adj[i][j]==1){
+                  visited[i][j] = 1;
+                  result[i][j]=0;
+                  q.push({i,j});
+                }
+            }
+        }
+        bfs(q,result,visited);
+        return result;
+    }
+};
