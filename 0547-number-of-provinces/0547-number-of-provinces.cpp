dbb2dac2953ca3d4bcc7d510cc521306db107d28
Aryan Singh <149005346+Aryan31457@users.noreply.github.com>
2024-12-30 12:52:46 +0530

    Time: 3 ms (46.73%), Space: 20 MB (38.4%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    void dfs(int i,vector<vector<int>>& adj,vector<int>&visited){
-        visited[i]=1;
-        int n=adj.size();
-        for(int x=0;x<n;x++){
-            if(adj[i][x]==1){
-            if(visited[x]==-1){
-                dfs(x,adj,visited);
+    void bfs(int i,vector<vector<int>>& adj,vector<int>&visited){
+          int n=adj.size();
+            queue<int>q;
+            q.push(i);
+            visited[i]=1;
+            while(!q.empty()){
+                int fronti=q.front();
+                q.pop();
+                for(int x=0;x<n;x++){
+                    if(adj[fronti][x]==1 && visited[x]==-1){
+                        q.push(x);
+                        visited[x]=1;
+                    }
+                }
             }
-            }
-        }
-        return;
+            return ;
     }
     int findCircleNum(vector<vector<int>>&adj) {
         int n=adj.size();
-        vector<int>visited(n,-1);
         int ans=0;
-        for(int i=0;i<n;i++){
+          vector<int>visited(n,-1);
+          for(int i=0;i<n;i++){
             if(visited[i]==-1){
-                dfs(i,adj,visited);
-                ans++;
+            ans++;
+            bfs(i,adj,visited);
             }
-        }
-        return ans;
+          }
+          return ans;
     }
 };
