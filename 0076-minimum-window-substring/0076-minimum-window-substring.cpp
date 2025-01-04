8a3fff3e3ebbd877e266e9fb61a870f03e0c38e8
Aryan Singh <149005346+Aryan31457@users.noreply.github.com>
2025-01-05 00:00:05 +0530

    Time: 7 ms (70.1%), Space: 11.5 MB (42.66%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        vector<int> mappi(128, 0);
-        for (char i : t) {
-            mappi[i]++;
-        }
-
-        int remain = t.size();
-        int left = 0, right = 0;
-        int mini = INT_MAX;
-        int start = 0;
-
-        while (right < s.size()) {
-            if (mappi[s[right]] > 0) {
-                remain--;
-            }
-            mappi[s[right]]--;
-            right++;
-
-            while (remain == 0) {
-                if (right - left < mini) {
-                    mini = right - left;
-                    start = left;
+    string minWindow(string s1, string s2) {
+      if(s2.size()>s1.size())return "";
+        unordered_map<char,int>mappi;
+        for(auto i:s2)mappi[i]++;
+        int mini=INT_MAX;
+        string ans="";
+        int s=0,e=0;
+        int n=s1.size();
+        int start=-1;
+        int req=mappi.size();
+        int cnt=0;
+        while(e<n){
+            mappi[s1[e]]--;
+            
+            if(mappi[s1[e]]==0)cnt++;
+            
+            while(s<=e && cnt==req){
+                if(e-s+1<mini){
+                    mini=e-s+1;
+                    start=s;
                 }
-
-                mappi[s[left]]++;
-                if (mappi[s[left]] > 0) {
-                    remain++;
+                mappi[s1[s]]++;
+                if(mappi[s1[s]]>0){
+                    cnt--;
                 }
-                left++;
+                s++;
             }
+            e++;
+            
         }
-
-        return mini == INT_MAX ? "" : s.substr(start, mini);
+        if(start!=-1){
+        ans=s1.substr(start,mini);
+        }
+        return ans;
     }
 };
