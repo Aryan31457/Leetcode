cefa786b38b54d4bb4d5399d31e5bc3629a1d014
Aryan Singh <149005346+Aryan31457@users.noreply.github.com>
2025-01-17 03:51:41 +0530

    Time: 12 ms (56.33%), Space: 70.7 MB (56.01%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int n=nums.size();
+        stack<int>s;
+        int smax=INT_MIN;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]<smax){
+                return true;
+            }
+            else {
+                while(!s.empty() && s.top()<nums[i]){
+                    smax=max(smax,s.top());
+                    s.pop();
+                }
+            } 
+            s.push(nums[i]);
+        }
+        return false;
+    }
+};
