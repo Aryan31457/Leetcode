76338390867a17175fd14ef170c1320544594fbd
Aryan Singh <149005346+Aryan31457@users.noreply.github.com>
2025-01-25 17:02:44 +0530

    Time: 92 ms (41.15%), Space: 174 MB (18.71%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
+
+    string solve(string s, int i,int j){
         int n=s.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        string t=s;
-        int maxi=0;
-        int i_idx=0;
-        reverse(t.begin(),t.end());
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(s[i]==t[j]){
-                    dp[i+1][j+1]+=dp[i][j]+1;
-                    if(dp[i+1][j+1]>maxi){
-                        int start=i-dp[i+1][j+1]+1;
-                        if(start==n-(j+1)){
-                        maxi=dp[i+1][j+1];
-                        i_idx=i;
-                        }
-                    }
-                }
-            }
+        if(n<=1){
+            return s;
         }
-        string ans=s.substr(i_idx-maxi+1,maxi);
-        // cout<<s.substr(i_idx-maxi+1,maxi);
+        while(i>=0 && j<n && s[i]==s[j]){
+            i--;
+            j++;
+        }
+        return s.substr(i+1,j-i-1);
+    }
+    string longestPalindrome(string s) {
+        string ans=s.substr(0,1);
+        int n=s.size();
+        for(int i=0;i<n-1;i++){
+        string odd=solve(s,i,i);
+        string even=solve(s,i,i+1);
+
+
+        if(odd.size()>ans.size())ans=odd;
+        if(even.size()>ans.size())ans=even;
+
+        }
         return ans;
 
     }
