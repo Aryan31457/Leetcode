eca45f80830ca89ebfc21937a83797d4c7980351
Aryan Singh <149005346+Aryan31457@users.noreply.github.com>
2025-01-25 17:30:49 +0530

    Time: 6 ms (42.84%), Space: 18.6 MB (50.43%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+
+        vector<int>outdegree(n);
+        queue<int>q;
+        unordered_map<int,vector<int>>adj;
+        int c=0;
+        for(int i=0;i<pre.size();i++){  
+                adj[pre[i][0]].push_back(pre[i][1]);
+                outdegree[pre[i][1]]++;
+        } 
+
+        for(int i=0;i<n;i++){
+            if(outdegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>ans;
+        while(!q.empty()){
+
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto i:adj[node]){
+                outdegree[i]--;
+                if(outdegree[i]==0){
+                    q.push(i);
+                }
+            }
+        }
+        reverse(ans.begin(),ans.end());
+
+          return ans.size() == n ? ans : vector<int>{};
+    }
+};
